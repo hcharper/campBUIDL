508002e42bb2961a73770916f88aa23ba5551907
hcharper <harrison.c.harper@gmail.com>
2026-02-15 11:05:38 -0700

    first contract for token finished!

--- 101Token.sol
+++ 101Token.sol
@@ -11,5 +11,25 @@ contract HarrisonToken{
 
     constructor() {
         // called when the contract is deployed (one time ONLY)
+        owner = msg.sender; // here in the constructor, it is whoever deployed the contract. unique case
+
+        balances[owner] = 1000; //mint the owner 1000 tokens
+    }
+
+    function tranfer(unit amount, address recipient) public {
+        // need 2 parameters 
+            // 1- how much you want to send
+            // 2 - recipient address
+
+        //check that the sender has the balance to send
+        require(balances[msg.sender] >= amount, "Not Enough Funds");
+
+        //check for over/underflow to ensure that end state is the desired result
+        require(balances[msg.sender] - amount ,= balances[msg.sender]);
+        require(balances[recipient] + amount >= balances[recipient])
+
+        // we also need to add to the recipient account, and subtract frrom the sender account
+        balances[msg.sender] -= amount; // ALWAYS SUBTRACT FIRST
+        balances[recipient] += amount;
     }
 }
